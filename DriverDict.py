1733377cfe11c39548d409ed897d602db05bf472
deneenjp <89109372+deneenjp@users.noreply.github.com>
2022-04-12 10:13:20 -0400

    Create DriverDict.py

---
+++ DriverDict.py
@@ -0,0 +1,22 @@
+import csv
+
+with open (#driver CSV files)
+    reader = csv.DictReader
+    for row in reader:
+        name = str(row["driver"])
+        driver[name] = {
+            "driver rating" = int()
+            "tire life" = float()
+            "lap times" = list(#all previous times on current race)
+        }
+            
+with open (#track CSV files)
+    reader = csv.DictReader
+    for row in reader:
+        name = str(row("trackname")
+        Track[name] = {
+            "conditions" = 
+            "track type" = #straight/turn-heavy
+        }
+              
+#Update these attributes                   
