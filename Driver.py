7346834c1da0d5f65ee0416571aaccc6be4e73e5
Chase Austin <chase7867@gmail.com>
2022-04-27 17:26:45 -0500

    Chase update

---
+++ Driver.py
@@ -0,0 +1,28 @@
+from msilib.schema import Class
+
+
+# Contains information about a driver
+
+
+class Driver:
+    def init(self):
+
+
+    def test(self):
+        # add names
+        nameStr = name.split()
+        self.first_name = nameStr[0]
+        self.last_name = nameStr[1]
+
+        # add team info
+
+        # add age
+
+        # add laps
+        self.laps = None
+        self.lap_count = laps.length()
+
+
+
+
+
